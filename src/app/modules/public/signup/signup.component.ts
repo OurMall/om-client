10eab7123ed4420df91814da18c52691892777c3
Brian Castro <bcastro421@misena.edu.co>
2022-05-18 17:40:46 -0500

    Advance: workspace creation module. BC

--- src/app/modules/public/signup/signup.component.ts
+++ src/app/modules/public/signup/signup.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
 
 	stepper!: number;
-	role!: string;
+	role: string = "seller";
 
 	constructor() {}
 
 	ngOnInit(): void {
 		this.stepper = 1;
+	}
+
+	incrementStep(): void {
+		if (this.stepper >= 3) this.stepper = 0;
+		this.stepper++;
 	}
 
 	changeCurrentStep(nextStep: number): void {
