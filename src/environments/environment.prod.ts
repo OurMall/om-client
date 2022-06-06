216f3707dc3a8de9f528807aa70211dcfaa031dc
Brian Castro <bcastro421@misena.edu.co>
2022-06-06 02:39:21 -0500

    Update production environment. BC

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	authorizationServer: {
-		endpoint: "https://om-oauth2-api.herokuapp.com/",
+		endpoint: "https://om-oauth2-api.herokuapp.com",
 		application_id: "well-known-client-production",
 		application_secret: "74807f0f24780b21aec0320a53593ebf9f8b08d4"
 	}
