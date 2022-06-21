1817356e4be9d28de31cb7664ce75b81d1d01e30
Brian Castro <bcastro421@misena.edu.co>
2022-06-21 10:51:12 -0500

    update: provitional secure endpoints. BC

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
 	production: true,
 	authorizationServer: {
-		endpoint: "http://api.our-mall.store",
-		websocket_endpoint: "http://api.our-mall.store",
+		//endpoint: "https://api.our-mall.store",
+		//websocket_endpoint: "https://api.our-mall.store",
+		endpoint: "https://om-oauth2-api.herokuapp.com",
+		websocket_endpoint: "https://om-oauth2-api.herokuapp.com",
 		application_id: "well-known-client-production",
 		application_secret: "74807f0f24780b21aec0320a53593ebf9f8b08d4"
 	}
